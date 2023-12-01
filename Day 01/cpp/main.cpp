aafdefc7e7943f4cdf776f00b17e79b477a692c0
rubenrubiom <rubensipala@gmail.com>
2023-12-01 14:29:13 +0100

    Day 1

---
+++ Day 01/cpp/main.cpp
@@ -0,0 +1,49 @@
+/**
+--- Day 1: Trebuchet?! ---
+https://adventofcode.com/2023/day/1
+
+*/
+
+#include <string>
+#include <vector>
+#include <format>
+#include <iostream>
+#include <fstream>
+#include <cstdint>
+#include <chrono>
+
+uint8_t get_calibration_value(const std::string &calibration)
+{
+    char firstDigit{}, lastDigit{};
+    for (auto &value : calibration)
+    {
+        if (isdigit(value))
+        {
+            if (!firstDigit)
+            {
+                firstDigit = value;
+            }
+            lastDigit = value;
+        }
+    }
+    return stoi(std::string{firstDigit, lastDigit});
+}
+
+int main()
+{
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    std::ifstream file("../input.txt");
+    int total{0};
+    std::string line;
+    while (std::getline(file, line))
+    {
+        total += get_calibration_value(line);
+    }
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+
+    std::cout << std::format("The sum is: {}\n", total);
+    std::cout << std::format("Execution time: {}", duration);
+}
---
+++ Day 01/rust/src/main.rs
@@ -0,0 +1,33 @@
+/*
+--- Day 1: Trebuchet?! ---
+https://adventofcode.com/2023/day/1
+
+*/
+
+use std::{
+    fs::File,
+    io::{self, BufRead},
+};
+
+fn get_calibration_value(calibration: String) -> u32 {
+    let mut digits = calibration.chars().filter(char::is_ascii_digit);
+    let first_digit: char = digits.next().unwrap_or('0');
+    let second_digit: char = digits.last().unwrap_or(first_digit);
+    format!("{first_digit}{second_digit}")
+        .parse()
+        .unwrap()
+}
+
+fn main() {
+    let start_time = std::time::Instant::now();
+    let file = File::open("../input.txt").unwrap();
+    let total: u32 = io::BufReader::new(file)
+        .lines()
+        .map_while(Result::ok)
+        .map(get_calibration_value)
+        .sum();
+
+    let end_time = std::time::Instant::now();
+    std::println!("The total sum is: {total}");
+    std::println!("Execution time {:?}", (end_time - start_time));
+}
